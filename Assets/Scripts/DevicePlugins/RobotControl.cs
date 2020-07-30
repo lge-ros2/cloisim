0ff724af552bffb68c02d5ab190a052e26a2f7ac
Hyunseok Yang <hyunseok7.yang@lge.com>
2020-07-30 19:18:33 +0900

    Modify misomSensor device and RobotControl plugin - Add Uss IR support - Code Refactoring RobotControl pluging

--- Assets/Scripts/DevicePlugins/RobotControl.cs
+++ Assets/Scripts/DevicePlugins/RobotControl.cs
@@ -12,77 +12,19 @@ public class RobotControl : DevicePlugin
 {
 	private MicomInput micomInput = null;
 	private MicomSensor micomSensor = null;
-	private SensorDevices.IMU imuSensor = null;
 
-	private Motor motorLeft = null;
-	private Motor motorRight = null;
-
-	public float wheelRadius = 0.0f; // in mether
-	public float divideWheelRadius = 0.0f;
 
 	protected override void OnAwake()
 	{
 		micomInput = gameObject.AddComponent<MicomInput>();
 		micomSensor = gameObject.AddComponent<MicomSensor>();
-
-		imuSensor = gameObject.GetComponentInChildren<SensorDevices.IMU>();
+		micomSensor.SetPluginParameter(parameters);
 	}
 
 	protected override void OnStart()
 	{
-		const float MM2M = 0.001f;
-		var updateRate = parameters.GetValue<float>("update_rate", 20);
-		micomSensor.SetUpdateRate(updateRate);
-
-		var kp = parameters.GetValue<float>("PID/kp");
-		var ki = parameters.GetValue<float>("PID/ki");
-		var kd = parameters.GetValue<float>("PID/kd");
-
-		var pidControl = new PID(kp, ki, kd);
-
-		var wheelBase = parameters.GetValue<float>("wheel/base") * MM2M;
-		wheelRadius = parameters.GetValue<float>("wheel/radius") * MM2M;
-		divideWheelRadius = 1.0f/wheelRadius; // for performacne.
-
-		var wheelNameLeft = parameters.GetValue<string>("wheel/location[@type='left']");
-		var wheelNameRight = parameters.GetValue<string>("wheel/location[@type='right']");
-
 		var debugging = parameters.GetValue<bool>("debug", false);
 		micomInput.EnableDebugging = debugging;
-
-		var motorFriction = parameters.GetValue<float>("wheel/friction/motor", 0.1f); // Currently not used
-		var brakeFriction = parameters.GetValue<float>("wheel/friction/brake", 0.1f); // Currently not used
-
-		var modelList = GetComponentsInChildren<ModelPlugin>();
-		foreach (var model in modelList)
-		{
-			// Debug.Log(model.name);
-			if (model.name.Equals(wheelNameLeft))
-			{
-				var jointWheelLeft = model.GetComponentInChildren<HingeJoint>();
-				motorLeft = new Motor("Left", jointWheelLeft, pidControl);
-
-				var wheelLeftBody = jointWheelLeft.gameObject.GetComponent<Rigidbody>();
-
-				// Debug.Log("joint Wheel Left found : " + jointWheelLeft.name);
-				// Debug.Log("joint Wheel Left max angular velocity : " + jointWheelLeft.gameObject.GetComponent<Rigidbody>().maxAngularVelocity);
-			}
-			else if (model.name.Equals(wheelNameRight))
-			{
-				var jointWheelRight = model.GetComponentInChildren<HingeJoint>();
-				motorRight = new Motor("Right", jointWheelRight, pidControl);
-
-				var wheelRightBody = jointWheelRight.gameObject.GetComponent<Rigidbody>();
-
-				// Debug.Log("joint Wheel Right found : " + jointWheelRight.name);
-				// Debug.Log("joint Wheel Right max angular velocity : " + jointWheelRight.gameObject.GetComponent<Rigidbody>().maxAngularVelocity);
-			}
-
-			if (motorLeft != null && motorRight != null)
-			{
-				break;
-			}
-		}
 
 		var txHashKey = MakeHashKey("MICOM", "_SENSOR");
 		if (!RegisterTxDevice(txHashKey))
@@ -102,33 +44,13 @@ public class RobotControl : DevicePlugin
 
 	void FixedUpdate()
 	{
-		var localRotation = transform.rotation;
-		if (imuSensor != null)
+		if (micomInput != null && micomSensor != null)
 		{
-			micomSensor.SetIMU(imuSensor);
-			micomSensor.SetAccGyro(localRotation.eulerAngles);
-			// Debug.LogFormat("{0} {1}", localRotation, imuSensor.GetOrientation());
+			var targetWheelVelocityLeft = micomInput.GetWheelVelocityLeft();
+			var targetWheelVelocityRight = micomInput.GetWheelVelocityRight();
+
+			micomSensor.SetMotorVelocity(targetWheelVelocityLeft, targetWheelVelocityRight);
 		}
-
-		float linearVelocityLeft = 0;
-		float linearVelocityRight = 0;
-
-		if (motorLeft != null)
-		{
-			var targetWheelVelocityLeft = micomInput.GetWheelVelocityLeft() * divideWheelRadius;
-			motorLeft.SetVelocityTarget(targetWheelVelocityLeft);
-			linearVelocityLeft = motorLeft.GetCurrentVelocity() * wheelRadius;
-		}
-
-		if (motorRight != null)
-		{
-			var targetWheelVelocityRight = micomInput.GetWheelVelocityRight() * divideWheelRadius;
-			motorRight.SetVelocityTarget(targetWheelVelocityRight);
-			linearVelocityRight = motorRight.GetCurrentVelocity() * wheelRadius;
-		}
-
-		// Debug.LogFormat("{0} {1}, {2} {3} | {4}", motorLeft.GetCurrentVelocity(), linearVelocityLeft, motorRight.GetCurrentVelocity(), linearVelocityRight,  wheelRadius);
-		micomSensor.SetOdomData(linearVelocityLeft, linearVelocityRight);
 	}
 
 	private void Sender()
--- Assets/Scripts/Devices/MicomSensor.cs
+++ Assets/Scripts/Devices/MicomSensor.cs
@@ -4,21 +4,141 @@
  * SPDX-License-Identifier: MIT
  */
 
+using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using messages = gazebo.msgs;
 
 public class MicomSensor : Device
 {
+	private PluginParameters parameters = null;
 	private messages.Micom micomSensorData = null;
+
+	private Motor motorLeft = null;
+	private Motor motorRight = null;
+
+	public SensorDevices.IMU imuSensor = null;
+	public List<SensorDevices.Sonar> ussSensors = null;
+	public List<SensorDevices.Sonar> irSensors = null;
+	// private List<SensorDevices.Magnet> magnetSensors = null;
+	// private List<SensorDevices.Switch> switchSensors = null;
+
+	public float wheelRadius = 0.0f; // in mether
+	private float divideWheelRadius = 0.0f; // for computational performacne.
 
 	protected override void OnAwake()
 	{
+		imuSensor = gameObject.GetComponentInChildren<SensorDevices.IMU>();
+
+		ussSensors = new List<SensorDevices.Sonar>();
+		irSensors = new List<SensorDevices.Sonar>();
+
+		deviceName = "MicomSensor";
 	}
 
 	protected override void OnStart()
 	{
-		deviceName = "MicomSensor";
+		const float MM2M = 0.001f;
+		var modelList = GetComponentsInChildren<ModelPlugin>();
+
+		var updateRate = parameters.GetValue<float>("update_rate", 20);
+		SetUpdateRate(updateRate);
+
+		var kp = parameters.GetValue<float>("PID/kp");
+		var ki = parameters.GetValue<float>("PID/ki");
+		var kd = parameters.GetValue<float>("PID/kd");
+
+		var pidControl = new PID(kp, ki, kd);
+
+		var wheelBase = parameters.GetValue<float>("wheel/base") * MM2M;
+		wheelRadius = parameters.GetValue<float>("wheel/radius") * MM2M;
+		divideWheelRadius = 1.0f/wheelRadius;
+
+		var wheelNameLeft = parameters.GetValue<string>("wheel/location[@type='left']");
+		var wheelNameRight = parameters.GetValue<string>("wheel/location[@type='right']");
+
+		var motorFriction = parameters.GetValue<float>("wheel/friction/motor", 0.1f); // Currently not used
+		var brakeFriction = parameters.GetValue<float>("wheel/friction/brake", 0.1f); // Currently not used
+
+		foreach (var model in modelList)
+		{
+			// Debug.Log(model.name);
+			if (model.name.Equals(wheelNameLeft))
+			{
+				var jointWheelLeft = model.GetComponentInChildren<HingeJoint>();
+				motorLeft = new Motor("Left", jointWheelLeft, pidControl);
+
+				var wheelLeftBody = jointWheelLeft.gameObject.GetComponent<Rigidbody>();
+
+				// Debug.Log("joint Wheel Left found : " + jointWheelLeft.name);
+				// Debug.Log("joint Wheel Left max angular velocity : " + jointWheelLeft.gameObject.GetComponent<Rigidbody>().maxAngularVelocity);
+			}
+			else if (model.name.Equals(wheelNameRight))
+			{
+				var jointWheelRight = model.GetComponentInChildren<HingeJoint>();
+				motorRight = new Motor("Right", jointWheelRight, pidControl);
+
+				var wheelRightBody = jointWheelRight.gameObject.GetComponent<Rigidbody>();
+
+				// Debug.Log("joint Wheel Right found : " + jointWheelRight.name);
+				// Debug.Log("joint Wheel Right max angular velocity : " + jointWheelRight.gameObject.GetComponent<Rigidbody>().maxAngularVelocity);
+			}
+
+			if (motorLeft != null && motorRight != null)
+			{
+				break;
+			}
+		}
+
+		if (parameters.GetValues<string>("uss/sensor", out var ussList))
+		{
+			foreach (var model in modelList)
+			{
+				foreach (var uss in ussList)
+				{
+					if (model.name.Equals(uss))
+					{
+						var sonarSensor = model.GetComponentInChildren<SensorDevices.Sonar>();
+						ussSensors.Add(sonarSensor);
+						// Debug.Log("ussSensor found : " + sonarSensor.name);
+					}
+				}
+				micomSensorData.uss.Distances = new uint[ussList.Count];
+			}
+		}
+
+		if (parameters.GetValues<string>("ir/sensor", out var irList))
+		{
+			foreach (var model in modelList)
+			{
+				foreach (var ir in irList)
+				{
+					if (model.name.Equals(ir))
+					{
+						var sonarSensor = model.GetComponentInChildren<SensorDevices.Sonar>();
+						irSensors.Add(sonarSensor);
+						// Debug.Log("irSensor found : " + sonarSensor.name);
+					}
+				}
+				micomSensorData.ir.Distances = new uint[irList.Count];
+			}
+		}
+
+		if (parameters.GetValues<string>("magnet/sensor", out var magnetList))
+		{
+			foreach (var model in modelList)
+			{
+				// TODO: to be implemented
+			}
+		}
+
+		if (parameters.GetValues<string>("bumper/sensor", out var bumperList))
+		{
+			foreach (var model in modelList)
+			{
+				// TODO: to be implemented
+			}
+		}
 	}
 
 	protected override IEnumerator MainDeviceWorker()
@@ -47,6 +167,8 @@ public class MicomSensor : Device
 		micomSensorData.Imu.LinearAcceleration = new messages.Vector3d();
 		micomSensorData.Accgyro = new messages.Micom.AccGyro();
 		micomSensorData.Odom = new messages.Micom.Odometry();
+		micomSensorData.uss = new messages.Micom.Uss();
+		micomSensorData.ir = new messages.Micom.Ir();
 	}
 
 	protected override void GenerateMessage()
@@ -56,18 +178,53 @@ public class MicomSensor : Device
 		PushData<messages.Micom>(micomSensorData);
 	}
 
-	public bool SetIMU(in SensorDevices.IMU imuSensor)
+	void FixedUpdate()
+	{
+		UpdateIMU();
+		UpdateAccGyro();
+		UpdateUss();
+		UpdateIr();
+	}
+
+	private void UpdateUss()
+	{
+		if ((micomSensorData == null || micomSensorData.uss == null))
+		{
+			return;
+		}
+
+		const float M2MM = 1000.0f;
+		var index = 0;
+		foreach (var uss in ussSensors)
+		{
+			micomSensorData.uss.Distances[index++] = (uint)(uss.GetDetectedRange() * M2MM);
+		}
+	}
+
+	private void UpdateIr()
+	{
+		if ((micomSensorData == null || micomSensorData.ir == null))
+		{
+			return;
+		}
+
+		const float M2MM = 1000.0f;
+		var index = 0;
+		foreach (var ir in irSensors)
+		{
+			micomSensorData.ir.Distances[index++] = (uint)(ir.GetDetectedRange() * M2MM);
+		}
+	}
+
+	private void UpdateIMU()
 	{
 		var imu = micomSensorData.Imu;
 
-		if (micomSensorData == null || imu == null)
+		if ((imuSensor == null) &&
+			(micomSensorData == null || imu == null) &&
+			(imu.Orientation == null || imu.AngularVelocity == null || imu.LinearAcceleration == null))
 		{
-			return false;
-		}
-
-		if (imu.Orientation == null || imu.AngularVelocity == null || imu.LinearAcceleration == null)
-		{
-			return false;
+			return;
 		}
 
 		var orientation = imuSensor.GetOrientation();
@@ -87,17 +244,18 @@ public class MicomSensor : Device
 		imu.LinearAcceleration.Z = linearAcceleration.z;
 
 		DeviceHelper.SetCurrentTime(micomSensorData.Imu.Stamp);
-
-		return true;
 	}
 
-	public bool SetAccGyro(in Vector3 angle)
+	private void UpdateAccGyro()
 	{
+		var localRotation = transform.rotation;
+		var angle = localRotation.eulerAngles;
+
 		var accGyro = micomSensorData.Accgyro;
 
 		if (micomSensorData == null || accGyro == null)
 		{
-			return false;
+			return;
 		}
 
 		accGyro.AngleX = angle.x;
@@ -111,11 +269,9 @@ public class MicomSensor : Device
 		accGyro.AngulerRateX = 0;
 		accGyro.AngulerRateY = 0;
 		accGyro.AngulerRateZ = 0;
-
-		return true;
 	}
 
-	public bool SetOdomData(in float linearVelocityLeft, in float linearVelocityRight)
+	private bool SetOdomData(in float linearVelocityLeft, in float linearVelocityRight)
 	{
 		const float M2MM = 1000.0f;
 
@@ -134,5 +290,27 @@ public class MicomSensor : Device
 		}
 
 		return false;
+	}
+
+	public void SetMotorVelocity(in float linearVelocityLeft, in float linearVelocityRight)
+	{
+		var angularVelocityLeft = linearVelocityLeft * divideWheelRadius;
+		var angularVelocityRight = linearVelocityRight * divideWheelRadius;
+
+		if (motorLeft != null && motorRight != null)
+		{
+			motorLeft.SetVelocityTarget(angularVelocityLeft);
+			motorRight.SetVelocityTarget(angularVelocityRight);
+
+			var linearJointVelocityLeft = motorLeft.GetCurrentVelocity() * wheelRadius;
+			var linearJointVelocityRight = motorRight.GetCurrentVelocity() * wheelRadius;
+
+			SetOdomData(linearJointVelocityLeft, linearJointVelocityRight);
+		}
+	}
+
+	public void SetPluginParameter(in PluginParameters pluginParams)
+	{
+		parameters = pluginParams;
 	}
 }
