cdb5da81275d89b4cd2c8dea9086bd91498970e1
Hyunseok Yang <hyunseok7.yang@lge.com>
2020-08-15 18:43:57 +0900

    Code cleanup in DeviceHelper

--- Assets/Scripts/Devices/DeviceHelper.cs
+++ Assets/Scripts/Devices/DeviceHelper.cs
@@ -106,10 +106,10 @@ public class DeviceHelper
 	{
 		// construct custom aspect ratio projection matrix
 		// math from https://www.scratchapixel.com/lessons/3d-basic-rendering/perspective-and-orthographic-projection-matrix/opengl-perspective-projection-matrix
-		float h = 1.0f / Mathf.Tan(hFov * Mathf.Deg2Rad / 2f);
-		float v = 1.0f / Mathf.Tan(vFov * Mathf.Deg2Rad / 2f);
-		float a = (far + near) / (near - far);
-		float b = (2.0f * far * near / (near - far));
+		var h = 1.0f / Mathf.Tan(hFov * Mathf.Deg2Rad / 2f);
+		var v = 1.0f / Mathf.Tan(vFov * Mathf.Deg2Rad / 2f);
+		var a = (far + near) / (near - far);
+		var b = (2.0f * far * near / (near - far));
 
 		var projMatrix = new Matrix4x4(
 			new Vector4(h, 0, 0, 0),
