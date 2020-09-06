314c9c1be4688ab0736ccede64453867fbeef515
Hyunseok Yang <hyunseok7.yang@lge.com>
2020-09-07 02:41:53 +0900

    add new ros2 info message transportation for info topic in RealSensePlugin

--- Assets/Scripts/DevicePlugins/DevicesPlugin.cs
+++ Assets/Scripts/DevicePlugins/DevicesPlugin.cs
@@ -8,48 +8,12 @@ using System.Collections.Generic;
 using System.Xml;
 using UnityEngine;
 
-public abstract class DevicesPlugin : MonoBehaviour, IDevicePlugin
+public abstract class DevicesPlugin : DevicePlugin
 {
-	public string pluginName { get; protected set; } = string.Empty;
-
-	protected PluginParameters parameters = new PluginParameters();
-
 	private Dictionary<string, DevicePlugin> devicePlugins = new Dictionary<string, DevicePlugin>();
-
-	protected abstract void OnAwake();
-	protected abstract void OnStart();
-
-	void Awake()
-	{
-		OnAwake();
-	}
-
-	void Start()
-	{
-		OnStart();
-	}
-
-	public void Reset() { }
 
 	public void AddDevicePlugin(in string deviceName, in DevicePlugin devicePlugin)
 	{
 		devicePlugins.Add(deviceName, devicePlugin);
-	}
-
-	public void SetPluginName(in string name)
-	{
-		pluginName = name;
-	}
-
-	public void SetPluginParameters(in XmlNode node)
-	{
-		if (parameters != null)
-		{
-			parameters.SetRootData(node);
-		}
-		else
-		{
-			Debug.LogWarning("Cannot set plugin parameters");
-		}
 	}
 }
