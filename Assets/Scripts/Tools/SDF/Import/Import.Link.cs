9e83283a4b90325ad13a13851070247d3498709b
Hyunseok Yang (YG) <hyunseok7.yang@lge.com>
2021-02-28 22:30:27 +0900

    Remove Matrix3x3 script
    remove calculating inertia matrix

--- Assets/Scripts/Tools/SDF/Import/Import.Link.cs
+++ Assets/Scripts/Tools/SDF/Import/Import.Link.cs
@@ -13,74 +13,23 @@ namespace SDF
 	{
 		public partial class Loader : Base
 		{
-			private static UE.Quaternion ToQuaternion(UE.Vector3 eigenvector0, UE.Vector3 eigenvector1, UE.Vector3 eigenvector2)
-			{
-				//From http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/
-				float tr = eigenvector0[0] + eigenvector1[1] + eigenvector2[2];
-				float qw, qx, qy, qz;
-				if (tr > 0)
-				{
-					float s = UE.Mathf.Sqrt(tr + 1.0f) * 2f; // S=4*qw
-					qw = 0.25f * s;
-					qx = (eigenvector1[2] - eigenvector2[1]) / s;
-					qy = (eigenvector2[0] - eigenvector0[2]) / s;
-					qz = (eigenvector0[1] - eigenvector1[0]) / s;
-				}
-				else if ((eigenvector0[0] > eigenvector1[1]) & (eigenvector0[0] > eigenvector2[2]))
-				{
-					float s = UE.Mathf.Sqrt(1.0f + eigenvector0[0] - eigenvector1[1] - eigenvector2[2]) * 2; // S=4*qx
-					qw = (eigenvector1[2] - eigenvector2[1]) / s;
-					qx = 0.25f * s;
-					qy = (eigenvector1[0] + eigenvector0[1]) / s;
-					qz = (eigenvector2[0] + eigenvector0[2]) / s;
-				}
-				else if (eigenvector1[1] > eigenvector2[2])
-				{
-					float s = UE.Mathf.Sqrt(1.0f + eigenvector1[1] - eigenvector0[0] - eigenvector2[2]) * 2; // S=4*qy
-					qw = (eigenvector2[0] - eigenvector0[2]) / s;
-					qx = (eigenvector1[0] + eigenvector0[1]) / s;
-					qy = 0.25f * s;
-					qz = (eigenvector2[1] + eigenvector1[2]) / s;
-				}
-				else
-				{
-					float s = UE.Mathf.Sqrt(1.0f + eigenvector2[2] - eigenvector0[0] - eigenvector1[1]) * 2; // S=4*qz
-					qw = (eigenvector0[1] - eigenvector1[0]) / s;
-					qx = (eigenvector2[0] + eigenvector0[2]) / s;
-					qy = (eigenvector2[1] + eigenvector1[2]) / s;
-					qz = 0.25f * s;
-				}
-				return new UE.Quaternion(qx, qy, qz, qw);
-			}
-
 			private UE.Pose GetInertiaTensor(in SDF.Inertial inertia)
 			{
-				 UE.Pose momentumInertiaTensor = UE.Pose.identity;
-				// Debug.LogWarningFormat("GetInertiaTensor: {0}, {1}, {2}", inertia.ixx, inertia.iyy, inertia.izz);
-				// var inertiaVector = new UE.Vector3((float)inertia.iyy, (float)inertia.izz, (float)inertia.ixx);
-				var inertiaMatrix = new Matrix3x3(
-																 (float)inertia.ixx, (float)inertia.ixy, (float)inertia.ixz,
-																 (float)inertia.ixy, (float)inertia.iyy, (float)inertia.iyz,
-																 (float)inertia.ixz, (float)inertia.iyz, (float)inertia.izz);
-
-				inertiaMatrix.DiagonalizeRealSymmetric(out var eigenvalues, out var eigenvectors);
-
-				// var inertiaVector = eigenvalues(inertia);
-				var quaternion = ToQuaternion(eigenvectors[0], eigenvectors[1], eigenvectors[2]);
-
-				momentumInertiaTensor.position = new UE.Vector3(eigenvalues.y, eigenvalues.z, eigenvalues.x);
-				momentumInertiaTensor.rotation = new UE.Quaternion(quaternion.y, -quaternion.z, -quaternion.x, quaternion.w);
-
+				UE.Pose inertiaMomentum = UE.Pose.identity;
+				var inertiaVector = new UE.Vector3((float)inertia.iyy, (float)inertia.izz, (float)inertia.ixx);
 				const float minimumInertiaTensor = 1e-6f;
 				for (var index = 0; index < 3; index++)
 				{
-					if (momentumInertiaTensor.position[index] <= minimumInertiaTensor)
+					if (inertiaVector[index] <= minimumInertiaTensor)
 					{
-						momentumInertiaTensor.position[index] = minimumInertiaTensor;
+						inertiaVector[index] = minimumInertiaTensor;
 					}
 				}
 
-				return momentumInertiaTensor;
+				inertiaMomentum.position = inertiaVector;
+				inertiaMomentum.rotation = UE.Quaternion.identity;
+
+				return inertiaMomentum;
 			}
 
 			protected override System.Object ImportLink(in SDF.Link link, in System.Object parentObject)
@@ -131,13 +80,15 @@ namespace SDF
 					articulationBody.centerOfMass = SDF2Unity.GetPosition(link.Inertial.pose.Pos);
 					articulationBody.jointType = UE.ArticulationJointType.FixedJoint;
 
+					articulationBody.ResetInertiaTensor();
+
 					var childCollider = articulationBody.transform.GetComponentInChildren<UE.Collider>();
 					if (childCollider != null && childCollider.transform.parent.Equals(articulationBody.transform))
 					{
-						articulationBody.ResetInertiaTensor();
+
 						var momentum = GetInertiaTensor(link.Inertial);
 						articulationBody.inertiaTensor = momentum.position;
-						// articulationBody.inertiaTensorRotation = momentum.rotation;
+						articulationBody.inertiaTensorRotation = momentum.rotation;
 						// articulationBody.inertiaTensorRotation = UE.Quaternion.Euler(0, 360, 0);
 					}
 					else
@@ -146,9 +97,6 @@ namespace SDF
 						articulationBody.inertiaTensorRotation = UE.Quaternion.identity;
 					}
 
-					// TODO: NOT Recommended to use innertia values from SDF
-					// articulationBody.inertiaTensor = GetInertiaTensor(link.Inertial);
-					// articulationBody.inertiaTensorRotation = Quaternion.identity;
 					// Debug.Log(linkObject.name + "  => Center Of Mass: " + articulationBody.centerOfMass.ToString("F6") + ", intertia: " + articulationBody.inertiaTensor.ToString("F6") + ", " + articulationBody.inertiaTensorRotation.ToString("F6"));
 					// Debug.Log("Create link body " + linkObject.name);
 				}
