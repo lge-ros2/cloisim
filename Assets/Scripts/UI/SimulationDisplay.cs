910767fd35e0f4c2e1a2ed016b058951d925e5c7
Hyunseok Yang <hyunseok7.yang@lge.com>
2021-04-29 03:02:00 +0900

    Separate the GUI part in Object spawning class

--- Assets/Scripts/UI/SimulationDisplay.cs
+++ Assets/Scripts/UI/SimulationDisplay.cs
@@ -11,6 +11,7 @@ using UnityEngine;
 public class SimulationDisplay : MonoBehaviour
 {
 	private Clock clock = null;
+	private ObjectSpawning _objectSpawning = null;
 	private string eventMessage = string.Empty;
 	private StringBuilder sbTimInfo = new StringBuilder();
 	private string _fpsString = string.Empty;
@@ -35,16 +36,33 @@ public class SimulationDisplay : MonoBehaviour
 
 	private Color logMessageColor = Color.red;
 
+	// private const int labelFontSize = 14;
+	private const int TextWidth = 45;
+	private Rect labelRect  = new Rect(Screen.width / 2 - TextWidth / 2, 10, TextWidth, 22);
+
+	private const float guiHeight = 25f;
+	private const float topMargin = 10f;
+	private const float toolbarWidth = 190f;
+	private string[] toolbarStrings = new string[] { "Box", "Cylinder", "Sphere" };
+
+	private string scaleFactorString = "0.5";
+	private int toolbarSelected = 0;
+
 	[Header("Rect")]
 	private Rect rectVersion = new Rect(textLeftMargin, textTopMargin, textWidthVersion, textHeight);
 	private Rect rectFps = new Rect(Screen.width - textWidthFps - textLeftMargin, textTopMargin, textWidthFps, textHeight);
 	private Rect rectSimulationinfo = new Rect(textLeftMargin, Screen.height - textHeight - textTopMargin, textWidthSimulation, textHeight);
 	private Rect rectLogMessage = new Rect(textLeftMargin, Screen.height - (textHeight*2) - textTopMargin, textWidthEvent, textHeight);
 
+	private string prevScaleFactorString;
+	private bool checkScaleFactorFocused = false;
+	private bool doCheckScaleFactorValue = false;
+
 	// Start is called before the first frame update
 	void Awake()
 	{
 		var coreObject = GameObject.Find("Core");
+		_objectSpawning = coreObject.GetComponent<ObjectSpawning>();
 		clock = DeviceHelper.GetGlobalClock();
 	}
 
@@ -110,7 +128,7 @@ public class SimulationDisplay : MonoBehaviour
 		return ("<b>" + value + "</b>");
 	}
 
-	void DrawShadow(in Rect rect, in string value)
+	private void DrawShadow(in Rect rect, in string value)
 	{
 		var prevColor = GUI.skin.label.normal.textColor;
 
@@ -123,10 +141,8 @@ public class SimulationDisplay : MonoBehaviour
 		GUI.skin.label.normal.textColor = prevColor;
 	}
 
-	void OnGUI()
+	private void DrawText()
 	{
-		var originLabelColor = GUI.skin.label.normal.textColor;
-
 		GUI.skin.label.alignment = TextAnchor.MiddleLeft;
 		GUI.skin.label.fontSize = labelFontSize;
 		GUI.skin.label.wordWrap = true;
@@ -162,6 +178,96 @@ public class SimulationDisplay : MonoBehaviour
 		DrawShadow(rectFps, _fpsString);
 		GUI.skin.label.normal.textColor = Color.cyan;
 		GUI.Label(rectFps, _fpsString);
+	}
+
+	private void DrawPropsMenus()
+	{
+		GUI.skin.label.alignment = TextAnchor.MiddleRight;
+		GUI.skin.label.fontSize = labelFontSize;
+		GUI.skin.label.alignment = TextAnchor.MiddleCenter;
+
+		var centerPointX = Screen.width / 2;
+
+		var rectToolbar = new Rect(centerPointX - toolbarWidth / 2, topMargin, toolbarWidth, guiHeight);
+		GUI.skin.label.normal.textColor = Color.white;
+		toolbarSelected = GUI.Toolbar(rectToolbar, toolbarSelected, toolbarStrings);
+
+		var rectToolbarLabel = rectToolbar;
+		rectToolbarLabel.x -= 45;
+		rectToolbarLabel.width = 45;
+
+		DrawShadow(rectToolbarLabel, "Props: ");
+		GUI.skin.label.normal.textColor = Color.white;
+		GUI.Label(rectToolbarLabel, "Props: ");
+
+		var rectScaleLabel = rectToolbar;
+		rectScaleLabel.x += (toolbarWidth + 7);
+		rectScaleLabel.width = 50;
+		DrawShadow(rectScaleLabel, "Scale: ");
+		GUI.skin.label.normal.textColor = Color.white;
+		GUI.Label(rectScaleLabel, "Scale: ");
+
+		var rectScale = rectScaleLabel;
+		rectScale.x += 50;
+		rectScale.width = 40;
+		GUI.SetNextControlName("ScaleField");
+		GUI.skin.textField.normal.textColor = Color.white;
+		GUI.skin.textField.alignment = TextAnchor.MiddleCenter;
+		scaleFactorString = GUI.TextField(rectScale, scaleFactorString, 5);
+
+		if (checkScaleFactorFocused && !GUI.GetNameOfFocusedControl().Equals("ScaleField"))
+		{
+			doCheckScaleFactorValue = true;
+			checkScaleFactorFocused = false;
+			// Debug.Log("Focused out!!");
+		}
+		else if (!checkScaleFactorFocused && GUI.GetNameOfFocusedControl().Equals("ScaleField"))
+		{
+			// Debug.Log("Focused!!!");
+			checkScaleFactorFocused = true;
+			prevScaleFactorString = scaleFactorString;
+		}
+
+		if (doCheckScaleFactorValue)
+		{
+			// Debug.Log("Do check!! previous " + prevScaleFactorString);
+			if (string.IsNullOrEmpty(scaleFactorString) )
+			{
+				scaleFactorString = prevScaleFactorString;
+			}
+			else
+			{
+				if (float.TryParse(scaleFactorString, out var scaleFactor))
+				{
+					if (scaleFactor < 0.1f)
+					{
+						scaleFactorString = "0.1";
+					}
+					else if (scaleFactor > 5f)
+					{
+						scaleFactorString = "5";
+					}
+				}
+				else
+				{
+					scaleFactorString = prevScaleFactorString;
+				}
+			}
+
+			doCheckScaleFactorValue = false;
+		}
+
+		_objectSpawning.SetPropType(toolbarSelected);
+		_objectSpawning.SetScaleFactor(scaleFactorString);
+	}
+
+	void OnGUI()
+	{
+		var originLabelColor = GUI.skin.label.normal.textColor;
+
+		DrawText();
+
+		DrawPropsMenus();
 
 		GUI.skin.label.normal.textColor = originLabelColor;
 	}
