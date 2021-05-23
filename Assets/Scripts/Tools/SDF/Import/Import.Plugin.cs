8fafb3580960adc805b907e62aed65f82af59841
Hyunseok Yang (YG) <hyunseok7.yang@lge.com>
2021-05-23 23:27:03 +0900

    separate SDF.Import.Loader and SDF.Import.Plugin
    Change method name in SDF.Plugin

---
+++ Assets/Scripts/Tools/SDF/Import/Import.Plugin.cs
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2021 LG Electronics Inc.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+using System;
+using UE = UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace SDF
+{
+	namespace Import
+	{
+		public partial class Loader : Base
+		{
+			protected override void ImportPlugin(in SDF.Plugin plugin, in System.Object parentObject)
+			{
+				var targetObject = (parentObject as UE.GameObject);
+
+				// filtering plugin name
+				var pluginLibraryName = plugin.LibraryName();
+				// Debug.Log("plugin name = " + pluginName);
+
+				var pluginType = Type.GetType(pluginLibraryName);
+				if (pluginType != null)
+				{
+					if (targetObject == null)
+					{
+						Debug.LogError("[Plugin] targetObject is empty");
+						return;
+					}
+
+					var pluginComponent = targetObject.AddComponent(pluginType);
+
+					var pluginObject = pluginComponent as CLOiSimPlugin;
+					var multiPluginObject = pluginComponent as CLOiSimMultiPlugin;
+
+					if (pluginObject != null)
+					{
+						var node = plugin.GetNode();
+						pluginObject.SetPluginParameters(plugin);
+						// Debug.Log("[Plugin] device added : " + plugin.Name);
+					}
+					else if (multiPluginObject != null)
+					{
+						var node = plugin.GetNode();
+						multiPluginObject.SetPluginParameters(plugin);
+						// Debug.Log("[Plugin] devices added : " + plugin.Name);
+					}
+					else
+					{
+						Debug.LogError("[Plugin] failed to add : " + plugin.Name);
+					}
+				}
+				else
+				{
+					Debug.LogWarningFormat("[Plugin] No plugin({0}) exist", plugin.Name);
+				}
+			}
+		}
+	}
+}
