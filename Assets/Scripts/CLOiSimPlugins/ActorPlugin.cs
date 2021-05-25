793fa0fa98164ad24c3025104d5b214f041638dd
Hyunseok Yang <hyunseok7.yang@lge.com>
2021-05-26 03:07:20 +0900

    Add function click to move on display using mouse right button after select the actor

--- Assets/Scripts/CLOiSimPlugins/ActorPlugin.cs
+++ Assets/Scripts/CLOiSimPlugins/ActorPlugin.cs
@@ -5,7 +5,6 @@
  */
 
 using System.Collections.Generic;
-using System.Collections;
 using UnityEngine;
 
 [DefaultExecutionOrder(600)]
---
+++ Assets/Scripts/UI/ActorControl.cs
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 LG Electronics Inc.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder(700)]
+public class ActorControl : MonoBehaviour
+{
+	private RaycastHit m_HitInfo = new RaycastHit();
+
+	private void ClickToMove(ref List<Transform> list)
+	{
+		foreach (var transform in list)
+		{
+			var actorAgent = transform.GetComponent<ActorAgent>();
+			if (actorAgent != null)
+			{
+				if (actorAgent.RandomWalking)
+				{
+					Debug.LogWarning("Cannot move for random walking actor");
+				}
+				else
+				{
+					actorAgent.AssignTargetDestination(m_HitInfo.point);
+				}
+			}
+		}
+	}
+
+	void LateUpdate()
+	{
+		if (Input.GetMouseButtonDown(1))
+		{
+			Main.Gizmos.GetSelectedTargets(out var list);
+
+			if (list.Count > 0)
+			{
+				var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
+				{
+					ClickToMove(ref list);
+				}
+			}
+		}
+	}
+}
