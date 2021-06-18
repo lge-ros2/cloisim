5cf6c0770f2772c33b2bcfd8bfab96102ba2983a
Hyunseok Yang <hyunseok7.yang@lge.com>
2021-06-18 12:55:42 +0900

    Modifiy Clock device to filter time dupliation

--- Assets/Scripts/Devices/Clock.cs
+++ Assets/Scripts/Devices/Clock.cs
@@ -11,11 +11,16 @@ public class Clock : Device
 {
 	private messages.WorldStatistics worldStat = null;
 
+#region Filter times
+	private messages.Time prevSimTime = new messages.Time();
+	private messages.Time prevRealTime = new messages.Time();
+#endregion
+
 	private double restartedSimTime = 0;
 	private double restartedRealTime = 0;
 
-	private double currentSimTime = 0;
-	private double currentRealTime = 0;
+	public double currentSimTime = 0;
+	public double currentRealTime = 0;
 
 	public double SimTime => currentSimTime;
 
@@ -23,8 +28,9 @@ public class Clock : Device
 
 	protected override void OnAwake()
 	{
-		Mode = ModeType.NONE;
-		DeviceName = "World Clock";
+		Mode = ModeType.TX_THREAD;
+		DeviceName = "WorldClock";
+		SetUpdateRate(20);
 	}
 
 	protected override void InitializeMessages()
@@ -39,10 +45,31 @@ public class Clock : Device
 	{
 		currentSimTime = Time.timeAsDouble - restartedSimTime;
 		currentRealTime = Time.realtimeSinceStartupAsDouble - restartedRealTime;
+	}
 
-		DeviceHelper.SetCurrentTime(worldStat.SimTime, false);
+	protected override void GenerateMessage()
+	{
+		DeviceHelper.SetCurrentTime(worldStat.SimTime);
 		DeviceHelper.SetCurrentTime(worldStat.RealTime, true);
-		PushDeviceMessage<messages.WorldStatistics>(worldStat);
+
+		// filter same clock info
+		if (prevSimTime.Sec.Equals(worldStat.SimTime.Sec) && prevSimTime.Nsec >= worldStat.SimTime.Nsec)
+		{
+			// Debug.LogWarningFormat("previous sim time is same {0}.{1} >= {2}.{3}", prevSimTime.Sec, prevSimTime.Nsec, worldStat.SimTime.Sec, worldStat.SimTime.Nsec);
+		}
+		else if (prevRealTime.Sec.Equals(worldStat.RealTime.Sec) && prevRealTime.Nsec >= worldStat.RealTime.Nsec)
+		{
+			// Debug.LogWarningFormat("previous real time is same {0}.{1} >= {2}.{3}", prevRealTime.Sec, prevRealTime.Nsec, worldStat.RealTime.Sec, worldStat.RealTime.Nsec);
+		}
+		else
+		{
+			PushDeviceMessage<messages.WorldStatistics>(worldStat);
+		}
+
+		prevSimTime.Sec = worldStat.SimTime.Sec;
+		prevSimTime.Nsec = worldStat.SimTime.Nsec;
+		prevRealTime.Sec = worldStat.RealTime.Sec;
+		prevRealTime.Nsec = worldStat.RealTime.Nsec;
 	}
 
 	public void ResetTime()
