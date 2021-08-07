b76bf7729d99930b6ac6f766edec9c96fa1b5f87
Hyunseok <hyunseok7.yang@lge.com>
2021-08-07 21:25:16 +0900

    CLOiSim-2.5.7
    [Major Changes]
    
    * Mesh: Support importing .fbx mesh file as an experimental
    * SDF.Implement: Revert commit 7aa6c3ee8122c96cdf8cd1644a6053731cc79760 -> Fix the SetParent for ArticulationBody.
    * SDF.Parser & SDF.Implement: parsing spring_reference and apply it for Joint
    
    [Minor Changes]
    
    * SDF.Implement: Update Project settings -> add new tag "geometry" and  apply "Geometry" tags
    * SDF.Import: Change Logging Level for Unity.Debug.Log in Import.Joint
    
    
    [Bug fix]
    
    * SensorDevice: Fix depth vertical range for 3d lidar sensor device
    * SDF.Implement: Fix Collision for collision object with unmerged mesh
    * SDF.Implement: Fix making combined mesh for optimized collision object

--- Assets/Scripts/Tools/SDF/Util/SDF2Unity.Mesh.cs
+++ Assets/Scripts/Tools/SDF/Util/SDF2Unity.Mesh.cs
@@ -76,7 +76,6 @@ public partial class SDF2Unity
 
 	public static Mesh MergeMeshes(in MeshFilter[] meshFilters)
 	{
-		var meshTransformMatrix = Matrix4x4.identity;
 		var combine = new CombineInstance[meshFilters.Length];
 		var totalVertexCount = 0;
 		for (var combineIndex = 0; combineIndex < meshFilters.Length; combineIndex++)
@@ -84,12 +83,36 @@ public partial class SDF2Unity
 			var meshFilter = meshFilters[combineIndex];
 			combine[combineIndex].mesh = meshFilter.sharedMesh;
 			totalVertexCount += meshFilter.sharedMesh.vertexCount;
-			combine[combineIndex].transform = Matrix4x4.identity;
-			// Debug.LogFormat("{0},{1}: {2}, {3}", meshFilter.name, meshFilter.transform.name, meshTranslation, meshRotation);
+			combine[combineIndex].transform = meshFilters[combineIndex].transform.localToWorldMatrix;
+			// Debug.LogFormat("{0}, {1}: {2}", meshFilter.name, meshFilter.transform.name, combine[combineIndex].transform);
 		}
 
 		var newCombinedMesh = new Mesh();
 		newCombinedMesh.indexFormat = (totalVertexCount >= System.UInt16.MaxValue) ? IndexFormat.UInt32 : IndexFormat.UInt16;
+		newCombinedMesh.CombineMeshes(combine, false, true);
+		newCombinedMesh.RecalculateNormals();
+		newCombinedMesh.RecalculateTangents();
+		newCombinedMesh.RecalculateBounds();
+		newCombinedMesh.Optimize();
+
+		return newCombinedMesh;
+	}
+
+	public static Mesh MergeMeshes(in MeshCollider[] meshColliders, in Matrix4x4 geometryWorldToLocalMatrix)
+	{
+		var combine = new CombineInstance[meshColliders.Length];
+		var totalVertexCount = 0;
+		for (var index = 0; index < meshColliders.Length; index++)
+		{
+			var meshCollider = meshColliders[index];
+			combine[index].mesh = meshCollider.sharedMesh;
+			totalVertexCount += combine[index].mesh.vertexCount;
+			var meshColliderTransform = meshCollider.transform;
+			combine[index].transform = geometryWorldToLocalMatrix * meshColliderTransform.localToWorldMatrix;
+		}
+
+		var newCombinedMesh = new Mesh();
+		newCombinedMesh.indexFormat = (totalVertexCount >= System.UInt16.MaxValue) ? UnityEngine.Rendering.IndexFormat.UInt32 : UnityEngine.Rendering.IndexFormat.UInt16;
 		newCombinedMesh.CombineMeshes(combine, false, true);
 		newCombinedMesh.RecalculateNormals();
 		newCombinedMesh.RecalculateTangents();
