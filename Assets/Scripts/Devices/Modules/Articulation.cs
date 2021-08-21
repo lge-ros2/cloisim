db2d071fceed62b4c3871cfacb7e83fae8268004
Hyunseok Yang (YG) <hyunseok7.yang@lge.com>
2021-08-21 19:08:11 +0900

    Modify controling articulation drive to ehnance a performance : remove .Equals for enum -> Boxing/Unboxing problem

--- Assets/Scripts/Devices/Modules/Articulation.cs
+++ Assets/Scripts/Devices/Modules/Articulation.cs
@@ -11,18 +11,18 @@ public class Articulation
 	public enum DriveType {NONE, FORCE_AND_VELOCITY, POSITION_AND_VELOCITY};
 	private DriveType driveType = DriveType.NONE;
 
-	private ArticulationBody jointBody = null;
-	private ArticulationJointType jointType = ArticulationJointType.FixedJoint;
+	private ArticulationBody _jointBody = null;
+	private ArticulationJointType _jointType = ArticulationJointType.FixedJoint;
 
-	public ArticulationJointType Type => jointType;
-	public GameObject gameObject => this.jointBody.gameObject;
+	public ArticulationJointType Type => _jointType;
+	public GameObject gameObject => _jointBody.gameObject;
 
 	public Articulation(in ArticulationBody jointBody)
 	{
 		if (jointBody != null)
 		{
-			this.jointBody = jointBody;
-			this.jointType = this.jointBody.jointType;
+			_jointBody = jointBody;
+			_jointType = _jointBody.jointType;
 		}
 	}
 
@@ -33,8 +33,8 @@ public class Articulation
 
 	public void Reset()
 	{
-		this.jointBody.velocity = Vector3.zero;
-		this.jointBody.angularVelocity = Vector3.zero;
+		_jointBody.velocity = Vector3.zero;
+		_jointBody.angularVelocity = Vector3.zero;
 	}
 
 	public void SetDriveType(in DriveType type)
@@ -44,52 +44,52 @@ public class Articulation
 
 	public bool IsRevoluteType()
 	{
-		return (Type.Equals(ArticulationJointType.RevoluteJoint) || Type.Equals(ArticulationJointType.SphericalJoint)) ? true : false;
+		return (Type == ArticulationJointType.RevoluteJoint || Type == ArticulationJointType.SphericalJoint) ? true : false;
 	}
 
 	protected void SetJointVelocity(in float velocity, in int targetDegree = 0)
 	{
-		if (this.jointBody != null)
+		if (_jointBody != null)
 		{
-			var jointVelocity = this.jointBody.jointVelocity;
+			var jointVelocity = _jointBody.jointVelocity;
 			jointVelocity[targetDegree] = velocity;
-			this.jointBody.jointVelocity = jointVelocity;
+			_jointBody.jointVelocity = jointVelocity;
 		}
 	}
 
 	private int GetValidIndex(in int index)
 	{
-		return (index >= this.jointBody.dofCount) ? (this.jointBody.dofCount - 1) : index;
+		return (index >= _jointBody.dofCount) ? (_jointBody.dofCount - 1) : index;
 	}
 
 	/// <returns>in radian for angular and in meters for linear</param>
 	public float GetJointPosition(int index = 0)
 	{
 		index = GetValidIndex(index);
-		return (this.jointBody == null || index == -1) ? 0 : this.jointBody.jointPosition[index];
+		return (_jointBody == null || index == -1) ? 0 : _jointBody.jointPosition[index];
 	}
 
 	/// <returns>torque for angular and force for linear</param>
 	public float GetJointForce(int index = 0)
 	{
 		index = GetValidIndex(index);
-		// Debug.Log(this.jointBody.name + ": " + this.jointBody.dofCount + ", " + this.jointBody.jointAcceleration[0] + ", " + this.jointBody.jointForce[0]);
-		return (this.jointBody == null || index == -1) ? 0 : this.jointBody.jointForce[index];
+		// Debug.Log(_jointBody.name + ": " + _jointBody.dofCount + ", " + _jointBody.jointAcceleration[0] + ", " + _jointBody.jointForce[0]);
+		return (_jointBody == null || index == -1) ? 0 : _jointBody.jointForce[index];
 	}
 
 	/// <returns>in radian for angular and in meters for linear</param>
 	public float GetJointVelocity(int index = 0)
 	{
 		index = GetValidIndex(index);
-		return (this.jointBody == null || index == -1) ? 0 : this.jointBody.jointVelocity[index];
+		return (_jointBody == null || index == -1) ? 0 : _jointBody.jointVelocity[index];
 	}
 
 	/// <returns>torque for angular and force for linear</param>
 	public float GetEffort()
 	{
-		var drive = DeviceHelper.GetDrive(ref this.jointBody);
+		var drive = GetDrive();
 		var F = drive.stiffness * (GetJointPosition() - drive.target) - drive.damping * (GetJointVelocity() - drive.targetVelocity);
-		// Debug.Log(this.jointBody.name + ": Calculated force = " + F);
+		// Debug.Log(_jointBody.name + ": Calculated force = " + F);
 		return F;
 	}
 
@@ -97,7 +97,7 @@ public class Articulation
 	/// <param name="targetVelocity">angular velocity in degrees per second.</param>
 	public void Drive(in float target, in float targetVelocity)
 	{
-		if (this.jointBody == null)
+		if (_jointBody == null)
 		{
 			Debug.LogWarning("ArticulationBody is empty, please set target body first");
 			return;
@@ -105,7 +105,7 @@ public class Articulation
 
 		// Arccording to document(https://docs.unity3d.com/2020.3/Documentation/ScriptReference/ArticulationDrive.html)
 		// F = stiffness * (currentPosition - target) - damping * (currentVelocity - targetVelocity).
-		var drive = DeviceHelper.GetDrive(ref this.jointBody);
+		var drive = GetDrive();
 
 		switch (this.driveType)
 		{
@@ -119,6 +119,135 @@ public class Articulation
 
 		drive.targetVelocity = targetVelocity;
 
-		DeviceHelper.SetDrive(ref this.jointBody, drive);
+		SetDrive(drive);
+	}
+
+	public ArticulationDrive GetDrive()
+	{
+		ArticulationDrive drive;
+
+		switch (_jointType)
+		{
+			case ArticulationJointType.RevoluteJoint:
+				drive = _jointBody.xDrive;
+				break;
+
+			case ArticulationJointType.PrismaticJoint:
+
+				if (_jointBody.linearLockX == ArticulationDofLock.LockedMotion &&
+					_jointBody.linearLockY == ArticulationDofLock.LockedMotion)
+				{
+					drive = _jointBody.zDrive;
+				}
+				else if (_jointBody.linearLockY == ArticulationDofLock.LockedMotion &&
+						 _jointBody.linearLockZ == ArticulationDofLock.LockedMotion)
+				{
+					drive = _jointBody.xDrive;
+				}
+				else if (_jointBody.linearLockX == ArticulationDofLock.LockedMotion &&
+						 _jointBody.linearLockZ == ArticulationDofLock.LockedMotion)
+				{
+					drive = _jointBody.yDrive;
+				}
+				else
+				{
+					Debug.LogWarning("Wrong Joint configuration!!! -> " + _jointBody.name);
+					goto default;
+				}
+				break;
+
+			case ArticulationJointType.SphericalJoint:
+
+				if (_jointBody.swingYLock == ArticulationDofLock.LockedMotion &&
+					_jointBody.swingZLock == ArticulationDofLock.LockedMotion)
+				{
+					drive = _jointBody.xDrive;
+				}
+				else if (_jointBody.swingYLock == ArticulationDofLock.LockedMotion &&
+						 _jointBody.twistLock == ArticulationDofLock.LockedMotion)
+				{
+					drive = _jointBody.zDrive;
+				}
+				else if (_jointBody.swingZLock == ArticulationDofLock.LockedMotion &&
+						 _jointBody.twistLock == ArticulationDofLock.LockedMotion)
+				{
+					drive = _jointBody.yDrive;
+				}
+				else
+				{
+					Debug.LogWarning("Wrong Joint configuration!!! -> " + _jointBody.name);
+					goto default;
+				}
+				break;
+
+			default:
+				Debug.LogWarning("unsupported joint type");
+				drive = new ArticulationDrive();
+				break;
+		}
+
+		return drive;
+	}
+
+	public void SetDrive(in ArticulationDrive drive)
+	{
+		switch (_jointType)
+		{
+			case ArticulationJointType.RevoluteJoint:
+				_jointBody.xDrive = drive;
+				break;
+
+			case ArticulationJointType.PrismaticJoint:
+
+				if (_jointBody.linearLockX == ArticulationDofLock.LockedMotion &&
+					_jointBody.linearLockY == ArticulationDofLock.LockedMotion)
+				{
+					_jointBody.zDrive = drive;
+				}
+				else if (_jointBody.linearLockY == ArticulationDofLock.LockedMotion &&
+						 _jointBody.linearLockZ == ArticulationDofLock.LockedMotion)
+				{
+					_jointBody.xDrive = drive;
+				}
+				else if (_jointBody.linearLockX == ArticulationDofLock.LockedMotion &&
+						 _jointBody.linearLockZ == ArticulationDofLock.LockedMotion)
+				{
+					_jointBody.yDrive = drive;
+				}
+				else
+				{
+					Debug.LogWarning("Wrong Joint configuration!!! -> " + _jointBody.name);
+				}
+
+				break;
+
+			case ArticulationJointType.SphericalJoint:
+
+				if (_jointBody.swingYLock == ArticulationDofLock.LockedMotion &&
+					_jointBody.swingZLock == ArticulationDofLock.LockedMotion)
+				{
+					_jointBody.xDrive = drive;
+				}
+				else if (_jointBody.swingYLock == ArticulationDofLock.LockedMotion &&
+						 _jointBody.twistLock == ArticulationDofLock.LockedMotion)
+				{
+					_jointBody.zDrive = drive;
+				}
+				else if (_jointBody.swingZLock == ArticulationDofLock.LockedMotion &&
+						 _jointBody.twistLock == ArticulationDofLock.LockedMotion)
+				{
+					_jointBody.yDrive = drive;
+				}
+				else
+				{
+					Debug.LogWarning("Wrong Joint configuration!!! -> " + _jointBody.name);
+				}
+
+				break;
+
+			default:
+				Debug.LogWarning("unsupported joint type");
+				break;
+		}
 	}
 }
