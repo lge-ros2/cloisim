65cc2a1f4d93dea5cfcd0fc236de8fbca8609286
Hyunseok <hyunseok7.yang@lge.com>
2022-02-23 21:52:29 +0900

    Enhance SDF Parser/Importer/Implement
    Modify and Update SDF.Parser, SDF.Importer And SDF.Implement
      - SDF.Root class
      - <pose>, <world>, <sensor>, <geometry>, <material>, <collision> and <joint>
    
    Possible to parse only for SDF elements
      - <scene>, <state>
    
    Modify Change "" => string.Empty

--- Assets/Scripts/Tools/SDF/Parser/ShapeTypes.cs
+++ Assets/Scripts/Tools/SDF/Parser/ShapeTypes.cs
@@ -19,7 +19,10 @@ namespace SDF
 
 	public class Cylinder : ShapeType
 	{
+		// Description: Radius of the cylinder
 		public double radius = 1;
+
+		// Description: Length of the cylinder along the z axis
 		public double length = 1;
 	}
 
@@ -50,6 +53,52 @@ namespace SDF
 	public class Ellipsoid : ShapeType
 	{
 		public Vector3<double> radii = new Vector3<double>();
+	}
+
+	// Description: A heightmap based on a 2d grayscale image.
+	public class HeightMap : ShapeType
+	{
+		// Description: The heightmap can contain multiple textures. The order of the texture matters. The first texture will appear at the lowest height, and the last texture at the highest height. Use blend to control the height thresholds and fade between textures.
+		public class Texture
+		{
+
+			// Description: Size of the applied texture in meters.
+			public double size = 10;
+
+			// Description: Diffuse texture image filename
+			public string diffuse = "__default__";
+
+			// Description: Normalmap texture image filename
+			public string normal = "__default__";
+		}
+
+		// Description: The blend tag controls how two adjacent textures are mixed. The number of blend elements should equal one less than the number of textures.
+		public class Blend
+		{
+			// Description: Min height of a blend layer
+			public double min_height = 0;
+			// Description: Distance over which the blend occurs
+			public double fade_dist = 0;
+		}
+
+		// Description: URI to a grayscale image file
+		public string uri = "__default__";
+
+		// Description: The size of the heightmap in world units. When loading an image: "size" is used if present, otherwise defaults to 1x1x1. When loading a DEM: "size" is used if present, otherwise defaults to true size of DEM.
+		public Vector3<double> size = new Vector3<double>(1, 1, 1);
+
+		// Description: A position offset.
+		public Vector3<double> pos = new Vector3<double>(0, 0, 0);
+
+		public List<Texture> texture = new List<Texture>();
+
+		public List<Blend> blend = new List<Blend>();
+
+		// Description: Set if the rendering engine will use terrain paging
+		public bool use_terrain_paging = false;
+
+		// Description: Samples per heightmap datum. For rasterized heightmaps, this indicates the number of samples to take per pixel. Using a higher value, e.g. 2, will generally improve the quality of the heightmap but lower performance.
+		public uint sampling = 1;
 	}
 
 	public class Image : ShapeType
