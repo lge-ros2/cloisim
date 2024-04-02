4517b2549fdf362c051bab3332a95682f1471d9e
Hyunseok Yang (YG) <hyunseok7.yang@lge.com>
2024-04-02 15:48:01 +0900

    Modfity SDf.Parse.Pose - add operator functions for Vector2 and Vector3

--- Assets/Scripts/Tools/SDF/Parser/Pose.cs
+++ Assets/Scripts/Tools/SDF/Parser/Pose.cs
@@ -10,11 +10,20 @@ using System;
 
 namespace SDF
 {
-	public class Vector2<T>
+	public class Vector
 	{
-		private static string regex_num_pattern = "[^.0-9Ee+-]";
-		protected static readonly Regex regex_num = new Regex(regex_num_pattern);
+		private static string regexNumPattern = "[^.0-9Ee+-]";
+		protected static readonly Regex regex = new Regex(regexNumPattern);
 
+		protected double StringToDouble(in string number)
+		{
+			var regexNumber = regex.Replace(number, string.Empty);
+			return double.Parse(regexNumber, NumberStyles.Float);
+		}
+	}
+
+	public class Vector2<T> : Vector
+	{
 		protected T _x;
 		protected T _y;
 
@@ -28,10 +37,9 @@ namespace SDF
 			FromString(value);
 		}
 
-		public Vector2(T x, T y)
+		public Vector2(in T x, in T y)
 		{
-			_x = x;
-			_y = y;
+			this.Set(x, y);
 		}
 
 		public T X
@@ -46,26 +54,36 @@ namespace SDF
 			set => _y = value;
 		}
 
-		public void Set(T x, T y)
+		public void Set(in T x, in T y)
 		{
 			_x = x;
 			_y = y;
 			// Console.WriteLine(typeof(T) + "::" + _x + ", " + _y);
 		}
 
-		public void Set(string x, string y)
+		public void Set(in string x, in string y)
 		{
 			var code = Type.GetTypeCode(typeof(T));
 			if (code != TypeCode.Empty)
 			{
-				x = regex_num.Replace(x, string.Empty);
-				y = regex_num.Replace(y, string.Empty);
-				var parsed_x = double.Parse(x, NumberStyles.Float);
-				var parsed_y = double.Parse(y, NumberStyles.Float);
-				// Console.WriteLine(typeof(T) + "::" + parsed_x + ", " + parsed_y);
-				Set((T)Convert.ChangeType(parsed_x, code), (T)Convert.ChangeType(parsed_y, code));
+				var parsedX = StringToDouble(x);
+				var parsedY = StringToDouble(y);
+				// Console.WriteLine(typeof(T) + "::" + parsedX + ", " + parsedY);
+				Set((T)Convert.ChangeType(parsedX, code), (T)Convert.ChangeType(parsedY, code));
 			}
 		}
+
+		public static Vector2<T> operator +(in Vector2<T> v)
+			=> v;
+
+		public static Vector2<T> operator -(Vector2<T> v)
+			=> new Vector2<T>(-(dynamic)v.X, -(dynamic)v.Y);
+
+		public static Vector2<T> operator +(in Vector2<T> left, in Vector2<T> right)
+			=> new Vector2<T>((dynamic)left.X + (dynamic)right.X, (dynamic)left.Y + (dynamic)right.Y);
+
+		public static Vector2<T> operator -(in Vector2<T> left, in Vector2<T> right)
+			=> left + (-right);
 
 		public void FromString(in string value)
 		{
@@ -101,7 +119,7 @@ namespace SDF
 			FromString(value);
 		}
 
-		public Vector3(T x, T y, T z)
+		public Vector3(in T x, in T y, in T z)
 		{
 			Set(x, y, z);
 		}
@@ -112,26 +130,40 @@ namespace SDF
 			set => _z = value;
 		}
 
-		public void Set(T x, T y, T z)
+		public void Set(in T x, in T y, in T z)
 		{
 			base.Set(x, y);
 			_z = z;
 			// Console.WriteLine(typeof(T) + "::" + _z);
 		}
 
-		public void Set(string x, string y, string z)
+		public void Set(in string x, in string y, in string z)
 		{
-			base.Set(x, y);
-
 			var code = Type.GetTypeCode(typeof(T));
 			if (!code.Equals(TypeCode.Empty))
 			{
-				z = regex_num.Replace(z, string.Empty);
-				var parsed_z = double.Parse(z, NumberStyles.Float);
-				// Console.WriteLine(typeof(T) + "::" + parsed_z);
-				_z = (T)Convert.ChangeType(parsed_z, code);
+				var parsedZ = StringToDouble(z);
+				// Console.WriteLine(typeof(T) + "::" + parsedZ);
+				base.Set(x, y);
+				_z = (T)Convert.ChangeType(parsedZ, code);
 			}
 		}
+
+		public static Vector3<T> operator +(in Vector3<T> v)
+			=> v;
+
+		public static Vector3<T> operator -(Vector3<T> v)
+			=> new Vector3<T>(-(dynamic)v.X, -(dynamic)v.Y, -(dynamic)v.Z);
+
+		public static Vector3<T> operator +(in Vector3<T> left, in Vector3<T> right)
+			=> new Vector3<T>(
+				(dynamic)left.X + (dynamic)right.X,
+				(dynamic)left.Y + (dynamic)right.Y,
+				(dynamic)left.Z + (dynamic)right.Z);
+
+		public static Vector3<T> operator -(in Vector3<T> left, in Vector3<T> right)
+			=> left + (-right);
+
 
 		new public void FromString(in string value)
 		{
