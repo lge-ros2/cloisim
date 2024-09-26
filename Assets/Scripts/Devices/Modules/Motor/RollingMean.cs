fcddd08e2709c27f83aab7b35e25eba5926958f6
Hyunseok Yang (YG) <hyunseok7.yang@lge.com>
2024-09-26 16:50:30 +0900

    Separate RollingMean class from Odometry class

---
+++ Assets/Scripts/Devices/Modules/Motor/RollingMean.cs
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020 LG Electronics Inc.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+using System.Collections.Generic;
+
+public class RollingMean
+{
+	private int _maxSize = 0;
+	private Queue<double> _accumulate = null;
+	private double _accumulateSum = 0f;
+
+	public RollingMean(in int windowSize = 5)
+	{
+		_maxSize = windowSize;
+		_accumulate = new Queue<double>(windowSize);
+	}
+
+	public void Accumulate(in double value)
+	{
+		if (_accumulate.Count == _maxSize)
+		{
+			_accumulateSum -= _accumulate.Dequeue();
+		}
+
+		_accumulate.Enqueue(value);
+		_accumulateSum += value;
+	}
+
+	public void Reset()
+	{
+		_accumulate.Clear();
+		_accumulateSum = 0;
+	}
+
+	public double Get()
+	{
+		return _accumulateSum / (double)_accumulate.Count;
+	}
+}
