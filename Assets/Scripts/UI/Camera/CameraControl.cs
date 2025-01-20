e20c6c01af7e1a8cbc830a5edf2f67c80a49a99d
Hyunseok Yang (YG) <hyunseok7.yang@lge.com>
2025-01-20 18:24:45 +0900

    Modify CameraControl - limit camera view rotation X considering gimbol lock issue - fix ChangeCameraView() handling -> set _terminateMoving false before call this function

--- Assets/Scripts/UI/Camera/CameraControl.cs
+++ Assets/Scripts/UI/Camera/CameraControl.cs
@@ -132,8 +132,10 @@ public abstract class CameraControl : MonoBehaviour
 	private void HandleMouseControl()
 	{
 		_lastMouse = Input.mousePosition - _lastMouse;
-		_lastMouse.Set(-_lastMouse.y * _camSens, _lastMouse.x * _camSens, 0);
-		_lastMouse.Set(transform.eulerAngles.x + _lastMouse.x, transform.eulerAngles.y + _lastMouse.y, 0);
+
+		var mousePoseX = transform.eulerAngles.x - _lastMouse.y * _camSens;
+		var mousePoseY = transform.eulerAngles.y + _lastMouse.x * _camSens;
+		_lastMouse.Set(mousePoseX, mousePoseY, 0);
 
 		// Mouse camera angle done.
 		if (Input.GetMouseButton(0))
@@ -199,31 +201,32 @@ public abstract class CameraControl : MonoBehaviour
 		{
 			// Debug.Log("rotate camera left here");
 			_lastMouse.y -= _edgeSensAccumlated;
-			transform.eulerAngles = _lastMouse;
 		}
 		else if (Input.mousePosition.x > Screen.width - _edgeWidth)
 		{
 			// Debug.Log("rotate camera right here");
 			_lastMouse.y += _edgeSensAccumlated;
-			transform.eulerAngles = _lastMouse;
 		}
 		else if (Input.mousePosition.y < _edgeWidth)
 		{
 			// Debug.Log("rotate camera down here");
 			_lastMouse.x += _edgeSensAccumlated;
-			transform.eulerAngles = _lastMouse;
 		}
 		else if (Input.mousePosition.y > Screen.height - _edgeWidth)
 		{
-			// Debug.Log("rotate camera up here");
 			_lastMouse.x -= _edgeSensAccumlated;
-			transform.eulerAngles = _lastMouse;
 		}
 		else
 		{
 			_edgeSensAccumlated = 0.0f;
-			transform.eulerAngles = _lastMouse;
 		}
+
+		var xRotation =
+			((_lastMouse.x >= -1 && _lastMouse.x < 90) ||
+			 (_lastMouse.x > 270 && _lastMouse.x <= 361)) ?
+				_lastMouse.x : transform.localRotation.eulerAngles.x;
+
+		transform.localRotation = Quaternion.Euler(xRotation, _lastMouse.y, 0);
 	}
 
 	private void Rotate()
@@ -299,6 +302,7 @@ public abstract class CameraControl : MonoBehaviour
 			StopCoroutine(_movingCoroutine);
 		}
 
+		_terminateMoving = false;
 		_movingCoroutine = StartCoroutine(ChangeCameraView(targetPose));
 	}
 
@@ -307,6 +311,7 @@ public abstract class CameraControl : MonoBehaviour
 		if (_terminateMoving && _movingCoroutine != null)
 		{
 			StopCoroutine(_movingCoroutine);
+			_movingCoroutine = null;
 			_terminateMoving = false;
 		}
 	}
