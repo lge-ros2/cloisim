2754692d399bc64db98da08a637344500cb27b11
Hyunseok Yang (YG) <hyunseok7.yang@lge.com>
2025-02-05 14:47:23 +0900

    Add new CLOiSimPlugin - RangePlugin, IRPlugin
    make RangePlugin to base class for IRPlugin and SonarPlugin

---
+++ Assets/Scripts/CLOiSimPlugins/IRPlugin.cs
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2025 LG Electronics Inc.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+
+public class IRPlugin : RangePlugin
+{
+	IRPlugin()
+	{
+		_radiationType = RadiationType.INFRARED;
+	}
+}
