114946b7a68cc2c229b6e95c562bf8b54ea44a5d
Hyunseok Yang <hyunseok7.yang@lge.com>
2026-01-09 14:38:32 +0900

    Modify ROS2Inspector - display additional information: physics data from rigid body or articulation body - modify Unity2SDF: add new method AsUnity() directly convert format - convert vector using Unity2SDF tool

--- Assets/Scripts/Editor/ROS2Inspector.cs
+++ Assets/Scripts/Editor/ROS2Inspector.cs
@@ -25,27 +25,70 @@ public class ROS2Inspector : Editor
 			targetTransform.CompareTag("Light"))
 		{
 			targetTransform.GetLocalPositionAndRotation(out var localPosition, out var localRotation);
-			var sdfPosition = Unity2SDF.Position(localPosition);
-			var sdfRotation = Unity2SDF.Rotation(localRotation);
-			var ros2Position = new Vector3((float)sdfPosition.X, (float)sdfPosition.Y, (float)sdfPosition.Z);
-			var ros2Rotation = new Vector3((float)sdfRotation.Roll, (float)sdfRotation.Pitch, (float)sdfRotation.Yaw);
+			var ros2Position = Unity2SDF.Position(localPosition).AsUnity();
+			var ros2Rotation = Unity2SDF.Vector(localRotation.eulerAngles).AsUnity();
 			var ros2RotationDegree = ros2Rotation * Mathf.Rad2Deg;
 
 			EditorGUILayout.Space();
-			EditorGUILayout.LabelField("ROS2 Coordinates", EditorStyles.boldLabel);
+			EditorGUILayout.LabelField("ROS2 Coordinates, ", EditorStyles.boldLabel);
+			EditorGUILayout.HelpBox("Rotation is displayed as Roll, Pitch, Yaw (Euler angles).", MessageType.None);
 
 			// GUI.enabled = false;
 			EditorGUILayout.Vector3Field("Position (m)", ros2Position);
-			EditorGUILayout.Vector3Field("Rotation(Roll, Pitch, Yaw) (rad)", ros2Rotation);
-			EditorGUILayout.Vector3Field("Rotation(Roll, Pitch, Yaw) (deg)", ros2RotationDegree);
+			EditorGUILayout.Vector3Field("Rotation (rad)", ros2Rotation);
+			EditorGUILayout.Vector3Field("Rotation (deg)", ros2RotationDegree);
 			// GUI.enabled = true;
 
 			if (GUILayout.Button("Copy pose"))
 			{
-				var poseText = $"{ros2Position.x.ToString("f10")} {ros2Position.y.ToString("f10")} {ros2Position.z.ToString("f10")} "
-							+ $"{ros2Rotation.x.ToString("f10")} {ros2Rotation.y.ToString("f10")} {ros2Rotation.z.ToString("f10")}";
+				var poseText = $"{ros2Position.x.ToString("0.##########")} {ros2Position.y.ToString("0.##########")} {ros2Position.z.ToString("0.##########")} "
+							+ $"{ros2Rotation.x.ToString("0.##########")} {ros2Rotation.y.ToString("0.##########")} {ros2Rotation.z.ToString("0.##########")}";
 				GUIUtility.systemCopyBuffer = poseText;
 				Debug.LogFormat("Pose '{0}' Copied for {1}", poseText, targetTransform.name);
+			}
+
+			var rb = targetTransform.GetComponent<Rigidbody>();
+			var ab = targetTransform.GetComponent<ArticulationBody>();
+			var bodyType = string.Empty;
+			
+			if (rb == null && ab == null)
+			{
+				return;
+			}
+			else if (ab != null)
+			{
+				bodyType = "ArticulationBody";
+			}
+			else
+			{
+				bodyType = "RigidBody";
+			}
+
+			EditorGUILayout.Space();
+			EditorGUILayout.LabelField($"Physics Info: {bodyType}", EditorStyles.boldLabel);
+			EditorGUILayout.HelpBox("Inertia Tensor Rotation is displayed as Roll, Pitch, Yaw (Euler angles).", MessageType.None);
+
+			if (rb != null)
+			{
+				EditorGUILayout.Vector3Field("Velocity", Unity2SDF.Vector(rb.velocity).AsUnity());
+				EditorGUILayout.Vector3Field("Angular Velocity", Unity2SDF.Vector(rb.angularVelocity).AsUnity());
+				EditorGUILayout.Vector3Field("Inertia Tensor", Unity2SDF.Vector(rb.inertiaTensor).AsUnity());
+				EditorGUILayout.Vector3Field("Inertia Tensor Rotation", Unity2SDF.Vector(rb.inertiaTensorRotation.eulerAngles).AsUnity());
+				EditorGUILayout.Vector3Field("Center of Mass (Local)", Unity2SDF.Vector(rb.centerOfMass).AsUnity());
+				EditorGUILayout.Vector3Field("Center of Mass (World)", Unity2SDF.Vector(rb.worldCenterOfMass).AsUnity());
+			}
+			else if (ab != null)
+			{
+				EditorGUILayout.Vector3Field("Velocity", Unity2SDF.Vector(ab.velocity).AsUnity());
+				EditorGUILayout.Vector3Field("Angular Velocity", Unity2SDF.Vector(ab.angularVelocity).AsUnity());
+				EditorGUILayout.Vector3Field("Inertia Tensor", Unity2SDF.Vector(ab.inertiaTensor).AsUnity());
+				EditorGUILayout.Vector3Field("Inertia Tensor Rotation", Unity2SDF.Vector(ab.inertiaTensorRotation.eulerAngles).AsUnity());
+				EditorGUILayout.Vector3Field("Center of Mass (Local)", Unity2SDF.Vector(ab.centerOfMass).AsUnity());
+				EditorGUILayout.Vector3Field("Center of Mass (World)", Unity2SDF.Vector(ab.transform.TransformPoint(ab.centerOfMass)).AsUnity());
+			}
+			else
+			{
+				EditorGUILayout.LabelField("No RigidBody or ArticulationBody exist.");
 			}
 		}
 	}
